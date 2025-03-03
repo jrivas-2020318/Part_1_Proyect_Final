8f218357ef365d13b091a09e900e5422ba0b76ad
jrivas-2020318 <104040662+jrivas-2020318@users.noreply.github.com>
2025-03-03 01:00:43 -0600

    Se corrige error con el validate.jwt.js en su funcion validateJwt

--- middlewares/validate.jwt.js
+++ middlewares/validate.jwt.js
@@ -1,18 +1,24 @@
 'use strict'
-
 import jwt from 'jsonwebtoken'
+import { existUserById } from '../helpers/db.validators.js'
 
-export const validateJwt = async(req, res, next)=>{
-    try{
+export const validateJwt = async (req, res, next) => {
+    try {
         let secretKey = process.env.SECRET_KEY
         let { authorization } = req.headers
-        if(!authorization) return res.status(401).send({message: 'Unauthorized'})
+        if (!authorization) {
+            return res.status(401).send({ message: 'Unauthorized' })
+        }
         let user = jwt.verify(authorization, secretKey)
+        const userExists = await existUserById(user.uid)
+        if (!userExists) {
+            return res.status(404).send({ message: 'User not found or removed' })
+        }
         req.user = user
         next()
-    }catch(err){
-        console.error(err)
-        return res.status(401).send({message: 'Invalid token or expired'})
+    } catch (err) {
+        console.error('JWT Error:', err);
+        return res.status(401).send({ message: 'Invalid token or expired' })
     }
 }
 
