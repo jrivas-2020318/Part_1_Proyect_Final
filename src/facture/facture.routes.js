81b9c0fdab3addf5900f2f29f213f847e01b615a
jrivas-2020318 <104040662+jrivas-2020318@users.noreply.github.com>
2025-03-03 23:11:25 -0600

    Se corrige ruta

--- src/facture/facture.routes.js
+++ src/facture/facture.routes.js
@@ -4,7 +4,7 @@ import { isAdmin, validateJwt } from '../../middlewares/validate.jwt.js'
 import { factureUpdateValidator, factureValidator } from '../../helpers/validators.js'
 
 const api = Router()
-api.post('/addfacture', [validateJwt, factureValidator], createFacture)
+api.post('/addfacture', [validateJwt], createFacture)
 api.put('/updatefacture/:id', [validateJwt, factureUpdateValidator, isAdmin], updateFactureStatus)
 api.get('/getall', [validateJwt], getFacturesByUser)
 
